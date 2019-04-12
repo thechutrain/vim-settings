d634629874cfb090b79d3199589ed230e824ab5e
thechutrain <chu.chuyalan@gmail.com>
2019-04-12 10:51:18 -0400

    fixed previous issue of not being able to properly move things around

--- setup_my_vim.sh
+++ setup_my_vim.sh
@@ -3,3 +3,10 @@
 echo "curling for Vimified repo ..."
 curl -L https://raw.github.com/zaiste/vimified/master/install.sh | sh
 echo "=== finished downloading vimified ==="
+
+cp after.vim vimified/
+echo "let g:vimified_packages = ['fancy', 'coding', 'color']" > vimified/local.vimrc
+
+ touch ../.vimrc
+ mv vimified/vimrc ../.vimrc
+mv vimified/ ../.vim
