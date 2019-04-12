9620f6038e978b6a612a10f4073cfcdc74382b77
thechutrain <chu.chuyalan@gmail.com>
2019-04-12 10:35:22 -0400

    vimified having errors

--- .vim/local.vimrc
+++
@@ -1,1 +0,0 @@
-let g:vimified_packages = ['general', 'coding', 'color']
---
+++ setup_my_vim.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "curling for Vimified repo ..."
+curl -L https://raw.github.com/zaiste/vimified/master/install.sh | sh
+echo "=== finished downloading vimified ==="
