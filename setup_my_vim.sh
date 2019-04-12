1a52ed8c3cfb7813893d656d5651ba6a0f9607d9
thechutrain <chu.chuyalan@gmail.com>
2019-04-12 11:37:05 -0400

    even more simple vim setup

--- setup_my_vim.sh
+++ setup_my_vim.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
-echo "curling for Vimified repo ..."
 curl -L https://raw.github.com/zaiste/vimified/master/install.sh | sh
-echo "=== finished downloading vimified ==="
 
-cp after.vim vimified/
-echo "let g:vimified_packages = ['fancy', 'coding', 'color']" > vimified/local.vimrc
+cd .vim
+echo "let g:vimified_packages = ['fancy', 'coding', 'color']" > local.vimrc
+cat<<EOF> ./after.vimrc
+"############################################################
+"KEY MAPS:
+""############################################################
+inoremap jk <ESC>
 
- touch ../.vimrc
- mv vimified/vimrc ../.vimrc
-mv vimified/ ../.vim
+"Changing cursor based on Vim mode:¬
+"let &t_SI = "\e[6 q"
+"let &t_EI = "\e[2 q"
+
+" SOURCE: https://stackoverflow.com/questions/6488683/how-do-i-change-the-vim-cursor-in-insert-normal-mode
+
+let &t_SI = "\<Esc>]50;CursorShape=1\x7"
+let &t_EI = "\<Esc>]50;CursorShape=0\x7"
+:autocmd InsertEnter * set cul
+:autocmd InsertLeave * set nocul
+EOF
